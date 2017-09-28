b89018d05faa08cfe1d2af506f4fa55de42d9d03
manhntdn <30980625+manhntdn@users.noreply.github.com>
2017-09-28 08:46:31 +0700

    Update SapXepMangTangDan.java

--- src/Training/SapXepMangTangDan.java
+++ src/Training/SapXepMangTangDan.java
@@ -2,6 +2,7 @@ package Training;
 
 public class SapXepMangTangDan {
 	public static void main(String[] args) {
+		/*you must enter elements from keyboard, gay!*/
 		int[] array = {2, 1, 3, 4, 5, 10, 7, 9, 6, 8};
 		for(int i = 0 ; i < array.length - 1; i ++) {
 			for(int j = i+1 ; j < array.length; j ++) {
